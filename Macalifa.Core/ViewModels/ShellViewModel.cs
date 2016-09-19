c1f0862a33aa1a4a051dd643d8452d30331f6675
theweavrs <abdullahatta31@gmail.com>
2016-09-19 23:30:06 +0500

    Added GPLv3 License header.

--- Macalifa.Core/ViewModels/ShellViewModel.cs
+++ Macalifa.Core/ViewModels/ShellViewModel.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/* 
+	Macalifa. A music player made for Windows 10 store.
+    Copyright (C) 2016  theweavrs (Abdullah Atta)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Windows.Storage.Streams;
@@ -42,6 +59,7 @@ namespace Macalifa.ViewModels
             get
             { if (_openSongCommand == null) { _openSongCommand = new RelayCommand(param => this.Open(param)); } return _openSongCommand; }
         }
+
         #region Constructor
         public ShellViewModel()
         {
@@ -82,7 +100,6 @@ namespace Macalifa.ViewModels
             {
                 LibVM.PlayCommand.Execute(toPlayFile);
             }
-            //toPlayFile.State = PlayerState.Playing;
         }
         public ThreadSafeObservableCollection<int> ShuffledCollection()
         {
