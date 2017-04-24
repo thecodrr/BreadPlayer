3f7f9da10b429817c1fbdb418bca0dce11fbe86a
theweavrs <abdullahatta31@gmail.com>
2017-04-24 23:36:14 +0500

    fixed #122

--- BreadPlayer.Views.UWP/Extensions/DependencyObjectExtensions.cs
+++ BreadPlayer.Views.UWP/Extensions/DependencyObjectExtensions.cs
@@ -12,7 +12,7 @@ namespace BreadPlayer.Extensions
             ColorAnimation col = new ColorAnimation();
             col.From = fromColor;// ((SolidColorBrush)App.Current.Resources["SystemControlBackgroundAccentBrush"]).Color;
             col.To = toColor;
-            col.Duration = new Duration(TimeSpan.FromSeconds(1));
+            col.Duration = new Duration(TimeSpan.FromSeconds(3));
 
             Storyboard zgo = new Storyboard();
             Storyboard.SetTarget(col, objAnimate);// (SolidColorBrush)App.Current.Resources["SystemControlBackgroundAccentBrush"]);
