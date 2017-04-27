60e92dd6e67e560ab085f12606e9607efa354743
theweavrs <abdullahatta31@gmail.com>
2017-04-27 12:33:43 +0500

    implement online artist bio and album tracks grabbing

---
+++ BreadPlayer.Views.UWP/ViewModels/NowPlayingViewModel.cs
@@ -0,0 +1,61 @@
+﻿using BreadPlayer.Web.Lastfm;
+using IF.Lastfm.Core.Api;
+using IF.Lastfm.Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreadPlayer.ViewModels
+{
+    public class NowPlayingViewModel : ViewModelBase
+    {
+        string artistBio;
+        public string ArtistBio
+        {
+            get => artistBio;
+            set => Set(ref artistBio, value);
+        }
+        ThreadSafeObservableCollection<LastTrack> albumTracks;
+        public ThreadSafeObservableCollection<LastTrack> AlbumTracks
+        {
+            get => albumTracks;
+            set => Set(ref albumTracks, value);
+        }
+        ThreadSafeObservableCollection<LastArtist> similarArtists;
+        public ThreadSafeObservableCollection<LastArtist> SimilarArtists
+        {
+            get => similarArtists;
+            set => Set(ref similarArtists, value);
+        }
+        private LastfmClient LastfmClient
+        {
+            get => new Lastfm().LastfmClient;
+        }
+        public NowPlayingViewModel()
+        {
+            GetArtistInfo("Adele");
+            GetAlbumInfo("Adele", "25");
+        }
+        private async void GetArtistInfo(string artistName)
+        {
+            var artistInfoResponse = await LastfmClient.Artist.GetInfoAsync(artistName, "en", true);
+            if(artistInfoResponse.Success)
+            {
+                LastArtist artist = artistInfoResponse.Content;
+                ArtistBio = artist.Bio.Content;
+                SimilarArtists = new ThreadSafeObservableCollection<LastArtist>(artist.Similar);
+            }
+        }
+        private async void GetAlbumInfo(string artistName, string albumName)
+        {
+            var albumInfoResponse = await LastfmClient.Album.GetInfoAsync(artistName, albumName, true);
+            if (albumInfoResponse.Success)
+            {
+                LastAlbum album = albumInfoResponse.Content;
+                AlbumTracks = new ThreadSafeObservableCollection<LastTrack>(album.Tracks);
+            }
+        }
+    }
+}
