1d1727c4876fc16f933fccad7c2588710ad9a198
theweavrs <abdullahatta31@gmail.com>
2017-04-30 17:53:01 +0500

    add metadata correction when fetching artist/album info.

--- BreadPlayer.Views.UWP/ViewModels/NowPlayingViewModel.cs
+++ BreadPlayer.Views.UWP/ViewModels/NowPlayingViewModel.cs
@@ -27,6 +27,8 @@ namespace BreadPlayer.ViewModels
         public string ProgressText { get => progressText; set => Set(ref progressText, value); }
         #endregion
 
+        public string CorrectArtist { get; set; }
+        public string CorrectAlbum { get; set; }
         string artistBio;
         public string ArtistBio
         {
@@ -45,17 +47,17 @@ namespace BreadPlayer.ViewModels
             get => similarArtists;
             set => Set(ref similarArtists, value);
         }
+        public ICommand RetryCommand { get; set; }
         private LastfmClient LastfmClient
         {
             get => new Lastfm().LastfmClient;
         }
         public NowPlayingViewModel()
         {
+            RetryCommand = new DelegateCommand(Retry);
             tokenSource = new CancellationTokenSource();
             token = tokenSource.Token;
-            //GetLyrics("Eminem Phenomenal");
-            InitInfo();
-
+            
             //the work around to knowing when the new song has started.
             //the event is needed to update the bio etc.
             Core.SharedLogic.Player.MediaChanging += (sender, e) =>
@@ -63,45 +65,46 @@ namespace BreadPlayer.ViewModels
                 Core.SharedLogic.Player.MediaStateChanged += Player_MediaStateChanged;
             };
         }
-
+        private async void Retry()
+        {
+            if (string.IsNullOrEmpty(CorrectAlbum) || string.IsNullOrEmpty(CorrectArtist))
+                return;
+            IsFailed = false;
+            await GetInfo(CorrectArtist, CorrectAlbum);
+        }
         private async void Player_MediaStateChanged(object sender, Events.MediaStateChangedEventArgs e)
         {
             if (e.NewState == Core.PlayerState.Playing)
             {
                 Core.SharedLogic.Player.MediaStateChanged -= Player_MediaStateChanged;
-                await InitInfo();
-               // await GetArtistInfo(Core.SharedLogic.Player.CurrentlyPlayingFile.LeadArtist);
+                await GetInfo(Core.SharedLogic.Player.CurrentlyPlayingFile.LeadArtist, Core.SharedLogic.Player.CurrentlyPlayingFile.Album);
             }
         }
         Task FetchInfoTask;
-            CancellationToken token;
+        CancellationToken token;
         CancellationTokenSource tokenSource;
         IAsyncOperation<LastResponse<LastArtist>> ArtistInfoOperation;
         IAsyncOperation<LastResponse<LastAlbum>> AlbumInfoOperation;
-        private async Task InitInfo()
+      
+        private async Task GetInfo(string artistName, string albumName)
         {
             await Core.SharedLogic.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
             {
                 if (FetchInfoTask?.IsCompleted == false)
                     tokenSource.Cancel();
-                FetchInfoTask = GetArtistInfo(Core.SharedLogic.Player.CurrentlyPlayingFile.LeadArtist, token);
-                await FetchInfoTask;                         
+                FetchInfoTask = GetArtistInfo(artistName, albumName, token);
+                await FetchInfoTask;
             });
         }
-
-        private async Task GetArtistInfo(string artistName, CancellationToken token)
+        private async Task GetArtistInfo(string artistName, string albumName, CancellationToken token)
         {
             ConnectionProfile InternetConnectionProfile = NetworkInformation.GetInternetConnectionProfile();
 
             if (InternetConnectionProfile != null)
             {
-                //check if there is any old operation running.
-                if (ArtistInfoOperation != null && token.IsCancellationRequested && ArtistInfoOperation.Status != AsyncStatus.Completed)
-                {
-                    //cancel old operiation
-                    ArtistInfoOperation.Cancel();
-                }
-
+                IsLoading = true;
+                await GetAlbumInfo(artistName, albumName, token);
+                CheckAndCancelOperation(ArtistInfoOperation, token);
                 ArtistInfoOperation = LastfmClient.Artist.GetInfoAsync(artistName, "en", true).AsAsyncOperation();
 
                 var artistInfoResponse = await ArtistInfoOperation;
@@ -110,23 +113,21 @@ namespace BreadPlayer.ViewModels
                     LastArtist artist = artistInfoResponse.Content;
                     ArtistBio = artist.Bio.Content;
                     SimilarArtists = new ThreadSafeObservableCollection<LastArtist>(artist.Similar);
+                    
                 }
                 else
                 {
                     IsFailed = true;
                     IsLoading = false;
                 }
-                await GetAlbumInfo(Core.SharedLogic.Player.CurrentlyPlayingFile.LeadArtist, Core.SharedLogic.Player.CurrentlyPlayingFile.Album, token);
+                if (string.IsNullOrEmpty(ArtistBio) || !AlbumTracks.Any())
+                    IsFailed = true;
+                IsLoading = false;
             }
         }
         private async Task GetAlbumInfo(string artistName, string albumName, CancellationToken token)
         {
-            //check if there is any old operation running.
-            if (AlbumInfoOperation != null && token.IsCancellationRequested && AlbumInfoOperation.Status != AsyncStatus.Completed)
-            {
-                //cancel old operiation
-                AlbumInfoOperation.Cancel();
-            }
+            CheckAndCancelOperation(AlbumInfoOperation, token);
             AlbumInfoOperation = LastfmClient.Album.GetInfoAsync(artistName, albumName, true).AsAsyncOperation();
             var albumInfoResponse = await AlbumInfoOperation;
             if (albumInfoResponse.Success)
@@ -138,7 +139,15 @@ namespace BreadPlayer.ViewModels
             {
                 IsFailed = true;
             }
-            IsLoading = false;
+        }
+        private void CheckAndCancelOperation<T>(IAsyncOperation<T> operation, CancellationToken token)
+        {
+            //check if there is any old operation running.
+            if (operation != null && token.IsCancellationRequested && operation.Status != AsyncStatus.Completed)
+            {
+                //cancel old operiation
+                operation.Cancel();
+            }
         }
         private async void GetLyrics(string query)
         {
