3108b8968ff9e8d3611d9b6fca41a02af20fc04f
theweavrs <abdullahatta31@gmail.com>
2017-05-01 17:23:19 +0500

    fix tap to play in now playing view

---
+++ BreadPlayer.Views.UWP/Converters/SymbolToCharConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace BreadPlayer.Converters
+{
+    public class SymbolToCharConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {            
+            return (char)(value as SymbolIcon).Symbol;
+        }
+        public object ConvertBack(object value, Type targetType,
+            object parameter, string language)
+        {           
+            return null;
+        }
+    }
+}
