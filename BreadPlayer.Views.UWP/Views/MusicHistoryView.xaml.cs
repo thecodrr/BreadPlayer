cbc0e35b8533fc55d8969b9b43d7ac5e893ece69
theweavrs <abdullahatta31@gmail.com>
2017-07-23 01:52:56 +0500

    cleaned up hamburger menu

---
+++ BreadPlayer.Views.UWP/Views/MusicHistoryView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace BreadPlayer
+{
+    public partial class MusicHistoryView : Page
+    {
+        public MusicHistoryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
