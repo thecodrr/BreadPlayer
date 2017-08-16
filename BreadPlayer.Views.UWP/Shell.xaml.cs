0bf6f4d7b31a49835d3d99cad55df5e0f8f82f61
theweavrs <abdullahatta31@gmail.com>
2017-08-16 20:18:15 +0500

    fixed a small typo

--- BreadPlayer.Views.UWP/Shell.xaml.cs
+++ BreadPlayer.Views.UWP/Shell.xaml.cs
@@ -79,7 +79,7 @@ namespace BreadPlayer
                     "• Fixed issue where many album arts were not loaded.\n" +
                     "• Fixed other bugs.\n\n" +
                     "𝐖𝐡𝐚𝐭'𝐬 𝐍𝐞𝐰:\n\n" +
-                    "• Added ability to ignore DRM-Protected songs. (𝑒𝑥𝑝𝑟𝑖𝑚𝑒𝑛𝑡𝑎𝑙)" +
+                    "• Added ability to ignore DRM-Protected songs. (𝑒𝑥𝑝𝑟𝑖𝑚𝑒𝑛𝑡𝑎𝑙)\n" +
                     "• Added sorting by tracknumber for album songs.\n";
                 await SharedLogic.NotificationManager.ShowMessageBoxAsync(releaseNotes, "What's new in v2.6.2");
                 RoamingSettingsHelper.SaveSetting("IsFirstTime", false);
