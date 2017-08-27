c6c692d9cb6d78c6d9f9be34424e425bc8835dc7
theweavrs <abdullahatta31@gmail.com>
2017-08-27 11:44:02 +0500

    added now playing toast sharing support

--- BreadPlayer.Views.UWP/Views/NowPlayingView.xaml.cs
+++ BreadPlayer.Views.UWP/Views/NowPlayingView.xaml.cs
@@ -11,6 +11,10 @@ using Windows.UI.Xaml.Media;
 using System.Threading.Tasks;
 using Windows.Foundation.Metadata;
 using BreadPlayer.Services;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+using System.Collections.Generic;
+using Windows.Storage.Streams;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace BreadPlayer
@@ -54,14 +58,10 @@ namespace BreadPlayer
         {
             VisualStateManager.GoToState(this, isMaximized ? "MinimizeState" : "MaximizeState", false);
             isMaximized = !isMaximized;
-            //if (isMaximized)
-            //{
-            //    RemoveLyricsList(lyricsGrid, trackControlPanel, "TextBrush");
-            //}
-            //else
-            //{
-            //    RemoveLyricsList(trackControlPanel, lyricsGrid, "ThemeForeground");
-            //}
+        }
+        private void OnShareSong(object sender, RoutedEventArgs e)
+        {
+            DataTransferManager.ShowShareUI();
         }
         private void RemoveLyricsList(Grid removeFrom, Grid removeTo, string foregroundColor)
         {
@@ -69,8 +69,39 @@ namespace BreadPlayer
             removeTo.Children.Add(lyricsList);
             ((SolidColorBrush)Resources["LyricsForeground"]).Color = ((SolidColorBrush)Application.Current.Resources[foregroundColor]).Color;
         }
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        public static Uri GetApplicationLink(string sharePageName)
         {
+            return new Uri("ms-sdk-sharesourcecs:navigate?page=" + sharePageName);
+        }
+    private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataTransferManager.IsSupported())
+            {
+                DataTransferManager manager = DataTransferManager.GetForCurrentView();
+                manager.DataRequested += async (s, args) =>
+                {
+                    var currentlyPlaying = SharedLogic.Player.CurrentlyPlayingFile;
+                    DataRequest dataRequest = args.Request;
+                    dataRequest.Data.Properties.Title = $"{currentlyPlaying.Title} by {currentlyPlaying.LeadArtist}";
+                    dataRequest.Data.Properties.Description = "Now Playing toast from Bread Player";
+                    dataRequest.Data.SetApplicationLink(GetApplicationLink(GetType().Name));
+                    dataRequest.Data.Properties.ContentSourceApplicationLink = GetApplicationLink(GetType().Name);
+
+                    if (!string.IsNullOrEmpty(currentlyPlaying.AttachedPicture))
+                    {
+                        var albumArt = await StorageFile.GetFileFromPathAsync(currentlyPlaying.AttachedPicture);
+                        List<IStorageItem> imageItems = new List<IStorageItem>();
+                        imageItems.Add(albumArt);
+                        dataRequest.Data.SetStorageItems(imageItems);
+
+                        RandomAccessStreamReference imageStreamRef = RandomAccessStreamReference.CreateFromFile(albumArt);
+                        dataRequest.Data.Properties.Thumbnail = imageStreamRef;
+                        dataRequest.Data.SetBitmap(imageStreamRef);
+                    }
+                    dataRequest.Data.SetText($"Now listening to {currentlyPlaying.Title} by {currentlyPlaying.LeadArtist}.\r\n\r\nGet BreadPlayer for your device: http://bit.ly/2wIqkHX");
+                    dataRequest.Data.SetWebLink(new Uri("http://bit.ly/2wIqkHX"));
+                };
+            }
             //events for providing seeking ability to the positon slider.
             Window.Current.CoreWindow.PointerPressed += (s, args) =>
             {
