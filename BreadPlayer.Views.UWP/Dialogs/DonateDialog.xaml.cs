09f8812e24b474b760689fe80943fb9ef4149648
theweavrs <abdullahatta31@gmail.com>
2017-10-13 16:30:54 +0500

    update donate dialog

--- BreadPlayer.Views.UWP/Dialogs/DonateDialog.xaml.cs
+++ BreadPlayer.Views.UWP/Dialogs/DonateDialog.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using BreadPlayer.Core;
+using BreadPlayer.Services;
 using BreadPlayer.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel.Store;
+using Windows.Services.Store;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -27,7 +28,7 @@ namespace BreadPlayer.Dialogs
             this.InitializeComponent();
         }
 
-        private async void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
             string productID = null;
             var shellVM = App.Current.Resources["ShellVM"] as ShellViewModel;
@@ -35,33 +36,67 @@ namespace BreadPlayer.Dialogs
                 shellVM.WatchAnAdCommand.Execute(null);
             else if (donate1.IsChecked == true)
             {
-                productID = "Donate2Dollars";
+                productID = "9p85s7w23ghc";
             }
             else if (donate2.IsChecked == true)
             {
-                productID = "Donate5Dollars";
+                productID = "9p7gkmjj249c";
             }
             else if (donate3.IsChecked == true)
             {
-                productID = "Donate10Dollars";
+                productID = "9pddqfm4rqtq";
             }
             if (productID != null)
             {
-                PurchaseResults purchaseResults = await CurrentAppSimulator.RequestProductPurchaseAsync(productID);
-                switch (purchaseResults.Status)
-                {
-                    case ProductPurchaseStatus.Succeeded:
-                        var transId = purchaseResults.TransactionId;
-                        await SharedLogic.Instance.NotificationManager.ShowMessageAsync("Thanks for the donation! Love you!");
-                        await CurrentAppSimulator.ReportConsumableFulfillmentAsync(productID, transId);
-                        break;
-                }
+                ConsumeAddOn(productID, 1);
             }
         }
+        private StoreContext context = null;
 
+        public async void ConsumeAddOn(string addOnStoreId, uint quantity)
+        {
+            if (context == null)
+            {
+                context = StoreContext.GetDefault();
+            }
+
+            Guid trackingId = Guid.NewGuid();
+            
+            StoreConsumableResult result = await context.ReportConsumableFulfillmentAsync(
+                addOnStoreId, quantity, trackingId);
+
+            // Capture the error message for the operation, if any.
+            string extendedError = string.Empty;
+            if (result.ExtendedError != null)
+            {
+                extendedError = result.ExtendedError.Message;
+            }
+
+            switch (result.Status)
+            {
+                case StoreConsumableStatus.Succeeded:
+                    await SharedLogic.Instance.NotificationManager.ShowMessageAsync("Thanks for the donation! Love you!");
+                    this.Hide();
+                    SplitViewMenu.SplitViewMenu.SelectPrevious();
+                    break;
+                case StoreConsumableStatus.NetworkError:
+                    await SharedLogic.Instance.NotificationManager.ShowMessageAsync("The fulfillment was unsuccessful due to a network error. " +
+                        "ExtendedError: " + extendedError);
+                    break;
+                case StoreConsumableStatus.ServerError:
+                    await SharedLogic.Instance.NotificationManager.ShowMessageAsync("The fulfillment was unsuccessful due to a server error. " +
+                        "ExtendedError: " + extendedError);
+                    break;
+                default:
+                    await SharedLogic.Instance.NotificationManager.ShowMessageAsync("The fulfillment was unsuccessful due to an unknown error. " +
+                         "ExtendedError: " + extendedError);
+                    break;
+            }
+        }
         private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
             this.Hide();
+            SplitViewMenu.SplitViewMenu.SelectPrevious();
         }
     }
 }
