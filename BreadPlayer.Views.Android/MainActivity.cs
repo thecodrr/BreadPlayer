f2c774ca685af13efbb24a55d878814079806c10
theweavrs <abdullahatta31@gmail.com>
2017-10-14 11:49:20 +0500

    add breadplayer android project

---
+++ BreadPlayer.Views.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace BreadPlayer.Views.Android
+{
+    [Activity(Label = "BreadPlayer.Views.Android", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
