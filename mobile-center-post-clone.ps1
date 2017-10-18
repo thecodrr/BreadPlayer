4ee01ce7f627db025e5632023587fca5a089d547
theweavr <theweavrs@users.noreply.github.com>
2017-10-18 21:29:15 +0500

    Update mobile-center-post-clone.ps1

--- mobile-center-post-clone.ps1
+++ mobile-center-post-clone.ps1
@@ -4,7 +4,7 @@ $msiPath = "$($env:MOBILECENTER_SOURCE_DIRECTORY)\MicrosoftStoreServicesSDK.msi"
  
 (New-Object Net.WebClient).DownloadFile('https://visualstudiogallery.msdn.microsoft.com/229b7858-2c6a-4073-886e-cbb79e851211/file/206533/6/MicrosoftStoreServicesSDK.msi', $msiPath) 
  
-cmd /c start /wait msiexec /i $msiPath
+cmd /c start /wait msiexec /i $msiPath /quiet /v
  
 Write-Host "Installed" -ForegroundColor green 
  
@@ -15,6 +15,6 @@ $msiPath = "$($env:MOBILECENTER_SOURCE_DIRECTORY)\MicrosoftAdvertisingSDK.msi"
  
 (New-Object Net.WebClient).DownloadFile('https://visualstudiogallery.msdn.microsoft.com/345ed584-cc35-4ff0-bbcf-25b31515ae8d/file/258419/5/MicrosoftAdvertisingSDK.msi', $msiPath) 
  
-cmd /c start /wait msiexec /i $msiPath 
+cmd /c start /wait msiexec /i $msiPath /quiet /v
  
 Write-Host "Installed" -ForegroundColor green 
