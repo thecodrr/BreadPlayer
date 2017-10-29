6693a4e7013905932d095ae9b4ab47c8c04a64af
theweavrs <abdullahatta31@gmail.com>
2017-10-29 22:36:46 +0500

    improve navigation in music history view

--- BreadPlayer.Views.UWP/Views/MusicHistoryView.xaml.cs
+++ BreadPlayer.Views.UWP/Views/MusicHistoryView.xaml.cs
@@ -15,7 +15,7 @@ namespace BreadPlayer
             this.DataContext = MusicHistoryVM;
         }
 
-        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        private async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (e.RemovedItems.Any())
                 (e.RemovedItems[0] as PivotItem).Content = null;
@@ -24,21 +24,13 @@ namespace BreadPlayer
             switch (mainPivot.SelectedIndex)
             {
                 case 0:
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed. Consider applying the 'await' operator to the result of the call.
-                    MusicHistoryVM.GetRecentlyPlayedSongs();
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed. Consider applying the 'await' operator to the result of the call.
+                    await MusicHistoryVM.GetRecentlyPlayedSongs().ConfigureAwait(false);
                     break;
-
                 case 1:
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed. Consider applying the 'await' operator to the result of the call.
-                    MusicHistoryVM.GetRecentlyAddedSongs();
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed. Consider applying the 'await' operator to the result of the call.
+                    await MusicHistoryVM.GetRecentlyAddedSongs().ConfigureAwait(false);
                     break;
-
                 case 2:
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed. Consider applying the 'await' operator to the result of the call.
-                    MusicHistoryVM.GetMostPlayedSongs();
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed. Consider applying the 'await' operator to the result of the call.
+                    await MusicHistoryVM.GetMostPlayedSongs().ConfigureAwait(false);
                     break;
             }
         }
