58ba45d546e093b77ac10de1fdf3bc974866ede5
theweavr <theweavrs@users.noreply.github.com>
2017-11-11 18:22:46 +0500

    Update vsoCert.ps1

--- BreadPlayer.Views.UWP/vsoCert.ps1
+++ BreadPlayer.Views.UWP/vsoCert.ps1
@@ -2,7 +2,7 @@ $password = 'allatonce123'
 
 Add-Type -AssemblyName System.Security
 $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-$cert.Import("$(CertPath)", $password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]"PersistKeySet")
+$cert.Import('$(CertPath)', $password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]"PersistKeySet")
 $store = new-object system.security.cryptography.X509Certificates.X509Store -argumentlist "MY", CurrentUser
 $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]"ReadWrite")
 $store.Add($cert)
